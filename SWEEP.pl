88ed086bdde6b2a974eb81e052429e3739cad8b6
jclev-uga <jclev@uga.edu>
2015-04-01 14:44:55 -0400

    Update SWEEP.pl

--- SWEEP.pl
+++ SWEEP.pl
@@ -1,3 +1,5 @@
+#!/usr/bin/env perl
+
 use strict;
 use warnings;
 use threads;
@@ -27,7 +29,12 @@ my $usage = <<__EOUSAGE__;
 #  -r <float>                minimum ratio of alternate allele to reference allele: default (0): 0 - 2               #
 #  -w <int>                  window size in bp: default (100) should be <= read length for optimal quality           #
 #  --no_cleanup              does not delete intermediate vcf files: default (FALSE)                                 #
-#                                                                                                                    #
+#  --ultimate                performs ultimate filtering for all homozygous calls                                    #
+#                            checks all reads mapped to base for any alterate allele (will take longer)              #
+#                            REQUIRES Biopython with pysam module installed in your path!!                           #
+#  -vcf <string>             Optional use an existing vcf file as input (bam files still need to be included)        #
+#  -num_genotypes <int>      if vcf file is used, enter number of genotypes                                          #
+#  -h                        help (usage)                                                                            #
 ######################################################################################################################
 #                                                                                                                    #
 #                                                                                                                    #
@@ -37,13 +44,11 @@ my $usage = <<__EOUSAGE__;
 #  (2) To do this easily you can add the path in your shell script                                                   #
 #  (3) alignment Bam files must be sorted and indexed                                                                #
 #  (4) include as many bam files as you need separated with '-b'                                                     #
-#  (5) the vcf file will include at the end of each line a p-value for a genotypic contingency test                  #
-#  (6) Example command line:                                                                                         #
+#  (5) Example command line:                                                                                         #
 #                                                                                                                    #
-#      perl SWEEP.pl -b gen1.sorted.bam -b gen2.sorted.bam -g genome.fa -o output.vcf -s 1 -d 5 -r 0.25              #
+#      perl SWEEP.pl -b gen1.sorted.bam -b gen2.sorted.bam -g genome.fa -o output.vcf -s 1 -d 5 -r 0.25        #
 #                                                                                                                    #
 ######################################################################################################################
-
 
 
 
@@ -51,7 +56,7 @@ __EOUSAGE__
 
     ;
 
-my @bamfile;
+my @bamfile=();
 my $genome;
 my $output;
 my $stringent=0;
@@ -60,8 +65,11 @@ my $lh;
 my $ratio=0;
 my $help_flag;
 my $no_cleanup = 0;
-my $contingency = 0;
+my $ultimate = 0;
 my $window = 100;
+my $contingency = 0;
+my $vcf = '';
+my$vcfnum = 0;
 
 &GetOptions ( 'h' => \$help_flag,
               'b=s@' => \@bamfile,
@@ -72,7 +80,9 @@ my $window = 100;
               'r:f' => \$ratio,
               'w:i' => \$window,
               'no_cleanup' => \$no_cleanup,
-              'geno_contingency_filter' => \$contingency
+              'ultimate' => \$ultimate,
+              'vcf=s' => \$vcf,
+              'num_genotypes:i' => \$vcfnum
               );
 
 if (@ARGV) {
@@ -95,28 +105,34 @@ main: {
      my $input_bam = join(' ', @bamfile);
      my $length = 0+@bamfile;
      my $depth = $filter*$length;
+     if ($vcf eq "") {
+         &process_cmd("samtools mpileup -u -f $genome $input_bam | bcftools view -bcv - > temp.raw.bcf");
+          &process_cmd("bcftools view temp.raw.bcf > prefilter.vcf");
+          &process_cmd("python scripts/Haplotype.py prefilter.vcf filter.vcf $length $window"); } else {
+          &process_cmd("python scripts/vcf.py $vcf filter.vcf $vcfnum $window");
+     }
 
-     &process_cmd("samtools mpileup -u -f $genome $input_bam | bcftools view -bcv - > temp.raw.bcf");
-     &process_cmd("bcftools view temp.raw.bcf > prefilter.vcf");
-     &process_cmd("python Haplotype.py prefilter.vcf filter.vcf $length $window");
-     &process_cmd("python FindSNP.py filter.vcf stringent.vcf $lh $length");
-     &process_cmd("python FilterDepth.py stringent.vcf $output $depth $ratio $contingency");
+
+     if ($ultimate) {
+          &process_cmd("python scripts/Homozygous.py $genome filter.vcf ultimate.vcf '$input_bam'");
+          &process_cmd("python scripts/FilterDepth.py ultimate.vcf $output $depth $ratio $contingency"); }
+
+     unless ($ultimate) {
+          &process_cmd("python scripts/FindSNP.py filter.vcf stringent.vcf $lh $length");
+          &process_cmd("python scripts/FilterDepth.py stringent.vcf $output $depth $ratio $contingency"); }
 
      unless ($no_cleanup) {
 
-     unlink "temp.raw.bcf";
+#     unlink "temp.raw.bcf";
      unlink "prefilter.vcf";
      unlink "filter.vcf";
      unlink "stringent.vcf";
      }
+     unless ($no_cleanup) { if ($vcf eq '') { unlink "temp.raw.bcf"; }}
 
      exit(0);
 
 }
-
-
-
-
 
 ####
 sub process_cmd {
